566cc12f6a4aa6786519a3c0d9fc58e850684ecc
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2023-10-27 12:50:12 +0200

    first commit

--- app/components/home/Opinion.js
+++ app/components/home/Opinion.js
@@ -6,7 +6,7 @@ function Opinion() {
     <section id="opinion" className="bg-black p-10">
     <div className="p-10 flex max-md:flex-wrap gap-5 max-w-[1400px] mx-auto">
       <div className="bg-slate-600 p-5 lg:w-[50%] h-fit">
-        <img src="/image/home/2.jpg" className="object-cover w-full"  alt="" />
+        <img src="/image/home/2.jpg" className="object-cover w-full"  alt="home" />
       </div>
       <div className="lg:w-[50%]">
         <h4 className="text-slate-600 font-bold">Cenimy zdanie klientów</h4>
@@ -18,8 +18,8 @@ function Opinion() {
         </p>
         <div className="bg-slate-600 mt-10 p-10 lg:ml-[-40%] lg:mr-[40%] max-sm:hidden hover:translate-x-10 transition-all ">
           <p className="text-white border-l-2 py-5 pl-5">
-            "Chciałbym polecić Wam firmę ACELGARAGE. Ich produkty są wykonane z
-            wysokiej jakości materiałów, co gwarantuje trwałość i odporność"
+          &#34;Chciałbym polecić Wam firmę ACELGARAGE. Ich produkty są wykonane z
+            wysokiej jakości materiałów, co gwarantuje trwałość i odporność&#34;
           </p>
           <p className="text-sm text-right text-white">Grzegorz Mąka</p>
         </div>
