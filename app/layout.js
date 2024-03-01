3b82614eaebbf61214f83e893987037e892f1f5c
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2024-03-01 08:39:56 +0100

    adding g-tag

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -22,8 +23,21 @@ export default function RootLayout({ children }) {
       className="scroll-smooth"
       style={{ scrollBehavior: "smooth" }}
     >
+    <head>
+    <script dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MH44M4M')`}}>
+
+    </script>
+
+    </head>
+
       <body className={inter.className}>
         <Header />
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH44M4M"
+height="0" width="0" ></iframe>
         <ToastContainer
           position="top-right"
           autoClose={5000}
