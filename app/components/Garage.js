164bf1465683062d543a51d9b7183084634718fa
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2025-02-03 19:27:14 +0100

    garage button

--- app/components/Garage.js
+++ app/components/Garage.js
@@ -16,21 +16,25 @@ function Garage({ src, title, link, prize }) {
         className="md:w-80 md:h-80  w-40 h-40 bg-cover bg-center hover:bg-right rounded-md shadow-white shadow-sm hover:shadow-md hover:shadow-white transition-all duration-500"
       >
         {/* ON HOVER SHOW BUTTON ,TITLE AND SHADOW ON BG */}
-        <div className="flex flex-col items-center justify-center w-full h-full bg-black/50 hover:bg-transparent transition-all duration-500">
-          <p className="text-white md:text-2xl font-bold text-center">
+        <p className="text-white md:text-2xl font-bold text-center bg-black/50">
             {title}
           </p>
-          {link ? (
-            <Link
-              href={link}
-              className="bg-white/50 hover:bg-white/80 text-black/50 hover:text-black/80 md:px-10 py-2 rounded-md transition-all duration-500"
-            >
-              {prize ? `${prize} zł` : "Zamów Wycenę"}
-            </Link>
-          ) : <Link href={src} target="_blank"  ><p className="btn-acel">Powiększ</p></Link>}
-        </div>
+        <div className="flex flex-col items-center justify-center w-full h-full bg-black/50 hover:bg-transparent transition-all duration-500">
+      
+    
         {/* ON HOVER SHOW BUTTON ,TITLE AND SHADOW ON BG */}
       </div>
+    
+        
+        </div>
+        {link ? (
+            <Link
+              href={link}
+              className="bg-white hover:bg-white/80 text-black/50 hover:text-black/80 md:px-10 py-2 rounded-b-md transition-all duration-500 w-full  text-center "
+            >
+              {prize ? `${prize} zł` : <p className="font-semibold">Zamów Wycenę</p>}
+            </Link>
+          ) : <Link href={src} target="_blank"  ><p className="btn-acel font-semibold">Powiększ</p></Link>}
     </div>
   );
 }
