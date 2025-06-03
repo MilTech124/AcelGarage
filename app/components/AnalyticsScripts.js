1b621e6b42b34d3179858c070d1a26165c305cac
Jaroslaw Matusiak <67241739+MilTech124@users.noreply.github.com>
2025-06-03 09:50:03 +0200

    upload

--- app/components/AnalyticsScripts.js
+++ app/components/AnalyticsScripts.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Script from 'next/script';
 
 const AnalyticsScripts = () => {
   return (
     <>
-      <script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"></script>
-      <script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="E8nUb-X5G5UeAm" async></script>
-      <script
+      <Script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js" strategy="afterInteractive" />
+      <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="E8nUb-X5G5UeAm" strategy="afterInteractive" />
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -13,7 +16,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MH44M4M')`,
         }}
-      ></script>
+      />
     </>
   );
 };
